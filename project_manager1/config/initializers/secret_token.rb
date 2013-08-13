423dc100635ee62bf248f175bbf6ea8786dc75e9
Elina Desaine <edesaine@ticklo.com>
2013-08-13 19:02:37 +0100

    initla commite

---
+++ project_manager1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectManager1::Application.config.secret_key_base = 'f796ca0c13f8832eb161de1d85c0049823ec473ca715e54dca3b60747af7c074edc5610863ed0a96d518e6efdea4b2dc9774c0bcbae657cb542d90d7115232c9'
